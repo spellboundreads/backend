8477e8a3a49337f013065acfa66fbe197bab7315
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-06-16 23:12:00 +0700

    feat: add TypeORM and PostgreSQL configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PGHOST,
+      port: parseInt(process.env.PGPORT || '5432'),
+      username: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE,
+      entities: [],
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
