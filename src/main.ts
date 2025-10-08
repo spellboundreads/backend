85decab5f7b6a10c8a67e331749a9aab93a12e02
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-08 19:21:57 +0700

    chore: change frontend port

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: `http://localhost:4000`,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     // credentials: true,
   });
