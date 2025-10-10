0fcca161215d70ac2160a0f4764ed4695522b897
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-10 20:57:49 +0700

    feat(filter): add new error handling

--- src/prisma-client-exception/prisma-client-exception.filter.ts
+++ src/prisma-client-exception/prisma-client-exception.filter.ts
@@ -32,6 +32,15 @@ export class PrismaClientExceptionFilter extends BaseExceptionFilter {
         });
         break;
       }
+
+      case 'P2025': {
+        const status = HttpStatus.NOT_FOUND;
+        response.status(status).json({
+          statusCode: status,
+          message: 'Record not found',
+        });
+        break;
+      }
       default:
         super.catch(exception, host);
         break;
