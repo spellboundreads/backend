cc88a4130b1ee02c1148882d2f4d7966e2f62e59
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-10 20:24:30 +0700

    feat(filters): implement exception filter to handle prisma errors

---
+++ src/prisma-client-exception/prisma-client-exception.filter.ts
@@ -0,0 +1,40 @@
+import { ArgumentsHost, Catch, HttpStatus } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { BaseExceptionFilter } from '@nestjs/core/';
+import { Response } from 'express';
+
+@Catch(Prisma.PrismaClientKnownRequestError)
+export class PrismaClientExceptionFilter extends BaseExceptionFilter {
+  catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
+    console.error(exception.message);
+
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    switch (exception.code) {
+      case 'P2002': {
+        const fields = (exception.meta?.target as string[]) || 'unknown field';
+
+        const status = HttpStatus.CONFLICT;
+        response.status(status).json({
+          statusCode: status,
+          message: `Duplicate value`,
+          fields,
+        });
+        break;
+      }
+
+      case 'P2001': {
+        const status = HttpStatus.NOT_FOUND;
+        response.status(status).json({
+          statusCode: status,
+          message: exception.message,
+        });
+        break;
+      }
+      default:
+        super.catch(exception, host);
+        break;
+    }
+  }
+}
