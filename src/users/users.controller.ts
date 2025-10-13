27d168f3d56ec1b2dbafae449f12e2fbb5233827
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-13 21:17:39 +0700

    feat(users): use guards in endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,43 +6,41 @@ import {
   Patch,
   Param,
   Delete,
-  Req,
   UseGuards,
   ParseUUIDPipe,
   NotFoundException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { PublicUserDto } from './dto/public-user.dto';
-// import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { UserEntity } from './entities/user.entity';
-import { ApiTags, ApiOkResponse, ApiCreatedResponse } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiOkResponse,
+  ApiCreatedResponse,
+  ApiBearerAuth,
+} from '@nestjs/swagger';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
+export const roundsOfHashing = 10;
 @Controller('users')
 @ApiTags('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  // @UseGuards(JwtAuthGuard)
-  // @Get('me')
-  // async getMe(@Req() req) {
-  //   const user = await this.usersService.findOne(req.user.id);
-  //   return plainToInstance(PublicUserDto, user, {
-  //     excludeExtraneousValues: true,
-  //   });
-  // }
+  @Post()
+  @ApiCreatedResponse({ type: UserEntity })
+  async create(@Body() data: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(data.password, roundsOfHashing);
+    data.password = hashedPassword;
 
-  // @UseGuards(JwtAuthGuard)
-  // @Patch('me')
-  // async updateMe(@Req() req, @Body() dto: UpdateUserDto) {
-  //   const updatedUser = await this.usersService.update(req.user.id, dto);
-  //   if (!updatedUser) throw new NotFoundException('User not found');
-  //   return plainToInstance(PublicUserDto, updatedUser, {
-  //     excludeExtraneousValues: true,
-  //   });
-  // }
+    return await this.usersService.create(data);
+  }
 
   @Get()
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiOkResponse({ type: UserEntity, isArray: true })
   async findAll() {
     const users = await this.usersService.findAll();
@@ -50,6 +48,8 @@ export class UsersController {
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiOkResponse({ type: UserEntity })
   async findOne(@Param('id', ParseUUIDPipe) id: string) {
     const user = await this.usersService.findOne(id);
@@ -58,14 +58,22 @@ export class UsersController {
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiCreatedResponse({ type: UserEntity })
-  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    const updated = await this.usersService.update(id, updateUserDto);
-    if (!updated) throw new NotFoundException('User not found');
-    return new UserEntity(updated);
+  async update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() data: UpdateUserDto,
+  ) {
+    if (data.password) {
+      data.password = await bcrypt.hash(data.password, roundsOfHashing);
+    }
+    return new UserEntity(await this.usersService.update(id, data));
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   async remove(@Param('id') id: string) {
     const deleted = await this.usersService.remove(id);
     if (!deleted) throw new NotFoundException('User not found');
