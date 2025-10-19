22f3a922994380bac962d3cbb0aa7232a5d1b196
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-20 03:15:56 +0700

    feat(auth): update JwtStrategy to extract JWT from cookies

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,6 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Request } from 'express';
 import { jwtSecret } from './auth.module';
 import { UsersService } from 'src/users/users.service';
 
@@ -8,7 +9,9 @@ import { UsersService } from 'src/users/users.service';
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(private usersService: UsersService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req: Request) => req?.cookies?.token,
+      ]),
       secretOrKey: jwtSecret,
     });
   }
