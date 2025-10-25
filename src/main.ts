52f43036c0044da0c5d2f1384de60dbcf68f9ee2
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-25 21:53:29 +0700

    fix(cors): remove the end / at frontend domain

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
 
   app.enableCors({
-    origin: `https://spellboundreads.vercel.app/`,
+    origin: `https://spellboundreads.vercel.app`,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
