94ab8e76c00bb4afbc216c9dd816e97114c370d7
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-10-27 15:33:03 +0700

    feat(db): only connection when used

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,9 +1,12 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    await this.$connect();
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleDestroy
+{
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
